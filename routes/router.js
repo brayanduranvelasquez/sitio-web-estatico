18e9d3cfa3ca01ae6e2ec4b21bbc8a25f1200406
Brayan DVelasquez <brayan13s133@gmail.com>
2020-11-08 21:02:24 -0430

    Las paginas cuentan de la misma plantilla y las tarjetas en el aside ahora son dinamicas
    De esta forma, se utiliza esa plantilla para contener toda la estructura de la pagina y se crea la informacion independiente de la misma. Y con respecto a las Tarjetas, van colocandose todas las publicaciones registradas.

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('base-de-datos');
+var publicaciones = [
+    {
+        viewNombre: 'base-de-datos',
+        titulo: '¿Que función cumplen las Bases de Datos?',
+        descripcion: 'Sin las bases de datos, no existirá la web como la conocemos.',
+        publicadoPor: 'Brayan Durán Velásquez',
+        imgPerfilUrl: '/img/perfiles/brayan-duran-velasquez.jpg',
+        imgUrl: '/img/base-de-datos/1.jpg',
+        diaPublicacion: 'El 07 de noviembre del 2020',
+    }
+];
+
+publicaciones.forEach(publicacion => {
+
+    router.get('/' + publicacion.viewNombre, (req, res) => {
+
+        res.render('index', {
+            viewNombre: publicacion.viewNombre,
+    
+            titulo: publicacion.titulo,
+            publicadoPor: publicacion.publicadoPor,
+            imgPerfilUrl: publicacion.imgPerfilUrl,
+            imgUrl: publicacion.imgUrl,
+            diaPublicacion: publicacion.diaPublicacion,
+    
+            publicaciones: publicaciones // Toda la informacion de las publicaciones, para ser usadar en las Tarjetas en el "aside"
+        });
+    })
+
 })
 
 module.exports = router;
