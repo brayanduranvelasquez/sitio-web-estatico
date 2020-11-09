b9b028f6bd1ee06bc2ad5ebd2b6edbf045b6f523
Brayan DVelasquez <brayan13s133@gmail.com>
2020-11-08 21:40:13 -0430

    Creada vista llamada javascript con su respectiva informacion

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-var publicaciones = [
-    {
+var publicaciones = [{
         viewNombre: 'base-de-datos',
         titulo: '¿Que función cumplen las Bases de Datos?',
         descripcion: 'Sin las bases de datos, no existirá la web como la conocemos.',
@@ -10,7 +9,17 @@ var publicaciones = [
         imgPerfilUrl: '/img/perfiles/brayan-duran-velasquez.jpg',
         imgUrl: '/img/base-de-datos/1.jpg',
         diaPublicacion: 'El 07 de noviembre del 2020',
-    }
+    },
+    {
+        viewNombre: 'javascript',
+
+        titulo: 'Aprendamos sobre que es Javascript',
+        descripcion: 'El lenguaje de programación de los navegadores.',
+        publicadoPor: 'Carlos E. Velásquez',
+        imgPerfilUrl: '/img/perfiles/carlos-matute.jpg',
+        imgUrl: '/img/javascript/1.jpg',
+        diaPublicacion: 'El 12 de marzo del 2020',
+    },
 ];
 
 publicaciones.forEach(publicacion => {
@@ -19,13 +28,13 @@ publicaciones.forEach(publicacion => {
 
         res.render('index', {
             viewNombre: publicacion.viewNombre,
-    
+
             titulo: publicacion.titulo,
             publicadoPor: publicacion.publicadoPor,
             imgPerfilUrl: publicacion.imgPerfilUrl,
             imgUrl: publicacion.imgUrl,
             diaPublicacion: publicacion.diaPublicacion,
-    
+
             publicaciones: publicaciones // Toda la informacion de las publicaciones, para ser usadar en las Tarjetas en el "aside"
         });
     })
